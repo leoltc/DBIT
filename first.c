0f6b9135a4866ca28e65de92b9ec9824c4d116f0
Leo Thomson <leothomsonltc@gmail.com>
2016-08-10 15:25:06 +0530

    2nd commit

--- first.c
+++ first.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 void main()
 {
-int a=5,b=10,c;
+int d,a=5,b=10,c;
 c=a+b;
-printf("The sum is %d",c);
+d=a-b;
+printf("The sum is %d \n",c); //hjfhjff
+printf("The difference is %d \n",d);
 }
